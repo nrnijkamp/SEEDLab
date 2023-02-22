d260c2eec3e2872b5264013705317c36e9a9a9f8
Dawson Gullickson <dgullickson@mines.edu>
2023-02-22 11:16:03 -0700

    Cleaned PID code, added timing

--- Mini Project/Sam/PIMotorImplementation/PIMotorImplementation.ino
+++ Mini Project/Sam/PIMotorImplementation/PIMotorImplementation.ino
@@ -2,37 +2,23 @@
 //Sam Leonard
 // 2/15/23
 //PID Control Gains 
-int Kp=15.3786175942488;
-int Ke=.5;
-int Ki=2.37803426483209;
-int Kd=0;
+double Kp = 15.3786175942488;
+double Ke = .5;
+double Ki = 2.37803426483209;
+double Kd = 0;
 
 //Given from document
-int I=0; //integral
-int D=0; //deriv
-int e_past=0; //prev val
-int Ts=0;
-int Tc=currentTime; //[Do they want me to reference a built in clock, or am I overthinking?]
-int long t=0;
+double I = 0; //integral
+double D = 0; //deriv
+double e_past = 0; //prev val
+double Ts = 0;
+double Tc = millis() / 1000; //[Do they want me to reference a built in clock, or am I overthinking?]
 
-int r=0; //radians
-int umax= 7.8; //max voltage that can be supplied
-void setup() {
-  // put your setup code here, to run once:
-
-}
+double r = 0; //radians
+double umax = 7.8; //max voltage that can be supplied
+void setup() {}
 
 void loop() {
-  // put your main code here, to run repeatedly:
-t++;
-//4.7 Part 3
-/*
-if(t=100)
-{
-  r=1;
-}
-
-*/
   //Given pseudocode translated to code
 
   //[read r - user input] radians [Speak to nick to learn how the system gets input]
@@ -40,36 +26,37 @@ if(t=100)
   //[read y - where the motor is at] radians, use encoder to find position?
 
   //calc error
-  int e = r-y; //find where it needs to move from where it is
+  double e = r-y; //find where it needs to move from where it is
 
   if(Ts>0)
   {
-    D=(e-e_past)/Ts; //derivative
-    e_past=e; //update val to get other vals
+    D = (e-e_past)/Ts; //derivative
+    e_past = e; //update val to get other vals
   }
   else 
   {
-    D=0;
+    D = 0;
   }
 
-  I=I+Ts*e; //integral
+  I = I+Ts*e; //integral
 
   //Calc controller output -- output voltage uses PID
- int u=Kp*e+Ki*I+Kd*D;
+  double u = Kp*e+Ki*I+Kd*D;
   //deals with actuator saturation i.e. if trying to write a voltage too high for board to supply
   if(abs(u)>umax) 
   {
-    u=sgn(u)*umax;
-    e=sgn(e)*min(umax/Kp, abs(e));
-    I=(u-Kp*e-Kd*D)/Ki; 
+    u = sgn(u)*umax;
+    e = sgn(e)*min(umax/Kp, abs(e));
+    I = (u-Kp*e-Kd*D)/Ki; 
   }
     //[write u to ouput]
 
     //part 3 implementation
-    Ts=currentTime-Tc;
-    Tc=currentTime;
+    double currentTime = millis() / 1000;
+    Ts = currentTime - Tc;
+    Tc = currentTime;
   Serial.print("Time: ");
-  Serial.print(t);
+  Serial.print(currentTime);
   Serial.print("R: ");
   Serial.println(r);
   }
