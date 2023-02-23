d1e34042f3341e425e55612597c7355d51b85e59
Dawson Gullickson <dgullickson@mines.edu>
2023-02-23 15:50:42 -0700

    Debugging python changes

--- Mini Project/pi_program.py
+++ Mini Project/pi_program.py
@@ -31,19 +31,9 @@ pi_camera.calibrate()
 # Program loop
 cap = pi_camera.video_init()
 while True:
-    # Get the quadrant the marker is in
     quadrant = pi_camera.video_loop(cap)
     print(quadrant)
-
-    # # Display angle
-    # angle_str = ["0", "pi/2", "pi", "3pi/2"][quadrant - 1]
-    # lcd.clear()
-    # lcd.message = "setpoint: " + angle_str
-
-    # # Send info to arduino
-    # angle = [0, 1, 2, 3][quadrant - 1]
-    # bus.write_byte(ADDRESS, angle)
-
     if pi_camera.was_quit_pressed():
         break
 pi_camera.video_deinit(cap)
+print("Done")
