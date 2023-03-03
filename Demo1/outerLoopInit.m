b0efa73fe1235f6e9214841bdf420c1c4e06fe25
Sam Leonard <87318421+ogel02@users.noreply.github.com>
2023-03-03 11:17:48 -0700

    outerloop implementation

---
+++ Demo1/outerLoopInit.m
@@ -0,0 +1,21 @@
+%Outer loop control - pt 5
+
+%Below is Motor variables
+K=.66666;
+sig=20;
+Kt=.5;
+Ke=.5;
+Ra=1;
+J=.05;
+b=.5;
+
+%run sim
+open_system('outerLoop');
+out=sim('outerLoop');
+
+%plot results
+figure
+plot(out.turnSpeed);
+
+
+% 3/3/23 untested implementation
