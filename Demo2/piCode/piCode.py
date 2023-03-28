4aadac745a46898a578cdd83f9a44b265bf2fbfd
Dawson Gullickson <dgullickson@mines.edu>
2023-03-28 15:37:12 -0600

    Added Demo 2 Python code

--- Demo2/piCode/piCode.py
+++ Demo2/piCode/piCode.py
@@ -42,7 +42,7 @@ def video_init() -> PiCamera:
     return camera
 
 # Obtains the marker's position
-def video_loop(camera: PiCamera) -> Union[float, None]:
+def video_loop(camera: PiCamera) -> tuple[float, float, float, int]:
     # https://picamera.readthedocs.io/en/release-1.13/api_array.html
     raw_capture = PiRGBArray(camera)
     camera.capture(raw_capture, "bgr")
@@ -54,33 +54,33 @@ def video_loop(camera: PiCamera) -> Union[float, None]:
     
     arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_5X5_1000)
     param = cv.aruco.DetectorParameters_create()
-    corners, _ids, _rejected = cv.aruco.detectMarkers(img, arucoDict, parameters = param)
+    corners, _ids, _rejected = cv.aruco.detectMarkers(img, arucoDict, parameters=param)
     
     # Find position and calculate angle
-    #We want the bottom center of the y part of the marker because I did all the calibraion on the ground, and I'm assuming that is where the angle is going to be measured in relation to the robot
-    angle = 0
-    xDistanceInFeet = 0
-    yDistanceInFeet = 0
+    # We want the bottom center of the y part of the marker because I did all the calibraion on the ground, and I'm assuming that is where the angle is going to be measured in relation to the robot
+    angle = 0.0
+    xDistanceInFeet = 0.0
+    yDistanceInFeet = 0.0
     if len(corners) >= 1:
-        xSum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
-        xCenterPixel = xSum/4
+        xSum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
+        xCenterPixel = xSum / 4
         
-        ySum = corners[0][0][2][1]+ corners[0][0][3][1]
-        yBottomCenterPixel = ySum/2
+        ySum = corners[0][0][2][1] + corners[0][0][3][1]
+        yBottomCenterPixel = ySum / 2
         
         #print("X center: ", x_centerPixel)
         #print("Y center: ", y_bottom_centerPixel)
         
         #Did a calibration picture with the angle that we have on the camera. This set of equations will convert pixel coors to real life feet away.
         #With real feet as our distance, we can find the real world angle
-        xPixelsPerFoot = 1.2982* yBottomCenterPixel + 215.55
-        xDistanceInFeet = (960 - xCenterPixel)/xPixelsPerFoot
+        xPixelsPerFoot = 1.2982 * yBottomCenterPixel + 215.55
+        xDistanceInFeet = (960 - xCenterPixel) / xPixelsPerFoot
         yDistanceInFeet = 184.39 * yBottomCenterPixel ** -0.746
 
-        angleInRad = math.atan(xDistanceInFeet/yDistanceInFeet)
+        angleInRad = math.atan(xDistanceInFeet / yDistanceInFeet)
         angle = angleInRad * 180 / math.pi
 
-        angle = round(angle,3)
+        angle = round(angle, 3)
 
 
         #print("Angle in rad:" , angleInRad)
@@ -90,7 +90,7 @@ def video_loop(camera: PiCamera) -> Union[float, None]:
         print("No markets detected")
 
     # Close raw_capture
-    #Is this why it's so slow? are we opening and closing the camera every time?
+    #NOTE Is this why it's so slow? are we opening and closing the camera every time?
     raw_capture.close()
         
     #Return angle
@@ -103,19 +103,14 @@ def video_deinit(camera: PiCamera):
 def was_quit_pressed() -> bool:
     return cv.waitKey(1) == ord('q')
 
-#Driver code
-#The angle variable is set the the angle the marker is at, when the video function is run
-#Press 'q' to exit the video mode
+# Driver code
+# Press 'q' to exit the video mode
 # Only runs when not imported
 if __name__ == "__main__":
     camera = video_init()
     while True:
-        result = video_loop(camera)
-        angle = result[0]
-        xDistanceInFeet = result[1]
-        yDistanceInFeet = result[2]
         #if detectMarker >= 1, there is a marker. If ==0, no marker
-        detectMarker = result[3]
+        angle, xDistanceInFeet, yDistanceInFeet, detectMarker = video_loop(camera)
         message = "Angle: {}".format(angle)
         print(message)
         lcd.clear()
---
+++ Demo2/pi_program.py
@@ -0,0 +1,91 @@
+# SEED Lab Demo 2 Group 3
+# Dawson J. Gullickson
+
+import math
+from typing import Optional
+
+import smbus2
+
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+import piCode.piCode as pi_camera
+
+# Chosen by the Arduino
+ADDRESS = 0x08
+
+# Initialize I2C Bus
+bus = smbus2.SMBus(1)
+
+# LCD Size
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialize I2C bus
+i2c = board.I2C()
+
+# Initialize LCD
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+
+# Function to send info to lcd and Arduino
+SEARCH_INST = 0
+MOVE_INST = 1
+MAX_DIST = 10
+def send_instruction(
+    searching: bool,
+    angle: Optional[float] = None,
+    distance: Optional[float] = None
+):
+    if searching:
+        bus.write_byte(ADDRESS, SEARCH_INST)
+        return
+    assert angle is not None
+    assert distance is not None
+
+    # Display angle
+    lcd.message = "Angle: {:.2}".format(angle)
+
+    # Convert data to bytes (0-255)
+    angle_byte = int((angle*255)//(2*math.pi)) % 256
+    distance_byte = int((distance*255)//MAX_DIST)
+    if distance_byte < 0: distance_byte = 0
+    if 255 < distance_byte: distance_byte = 255
+
+    # Send info to arduino
+    bus.write_byte(ADDRESS, MOVE_INST)
+    bus.write_byte(ADDRESS, angle_byte)
+    bus.write_byte(ADDRESS, distance_byte)
+
+# Calibrate camera
+camera = pi_camera.video_init()
+
+# Send initial searching instruction
+send_instruction(True)
+
+# Program loop
+searching = True
+last_angle = 0.0
+last_dist = 0.0
+while True:
+    # Get the quadrant the marker is in
+    angle, x_dist, y_dist, corners = pi_camera.video_loop(camera)
+
+    # Send instruction
+    if corners == 0:
+        # No markers detected; start searching unless already searching
+        if (searching): continue
+        send_instruction(True)
+        searching = True
+    else:
+        # Markers detected; send info unless only small change
+        dist = math.sqrt(x_dist**2 + y_dist**2)
+        if (abs(dist - last_dist) < 0.1): continue
+        if (abs(angle - last_angle) < 0.1): continue
+        send_instruction(False, angle, dist)
+        searching = False
+        last_angle = angle
+        last_dist = dist
+
+    if pi_camera.was_quit_pressed():
+        break
+pi_camera.video_deinit(camera)
